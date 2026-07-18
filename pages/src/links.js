657624fa348d255f27d44ec6ef4c525c6858ddfa
cesarbenjamindotnet <cesarbenjamindotnet@gmail.com>
2026-07-18 02:47:55 -0600

    Add links.js for repository and resource URLs

---
+++ pages/src/links.js
@@ -0,0 +1,10 @@
+// Central place for links to the fork's repository and resources.
+//
+// NOTE: there is no Sphinx docs publishing (GitHub Pages / Read the Docs)
+// configured for this repo yet (checked docs/conf.py and .github/workflows/
+// as of writing). Until that's set up, "docs" points at the docs/ folder
+// on GitHub itself.
+export const repoUrl = "https://github.com/xiidigital/xii-django-river-admin";
+export const docsUrl = `${repoUrl}/tree/master/docs`;
+export const contributorsUrl = `${repoUrl}/blob/master/CONTRIBUTORS`;
+export const licenseUrl = `${repoUrl}/blob/master/LICENSE`;
